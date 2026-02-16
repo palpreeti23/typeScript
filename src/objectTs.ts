a9cf01b1135b27101c461622622e077ba86537de
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2026-02-16 22:07:24 +0530

    chore: practice object types in TS

--- src/objectTs.ts
+++ src/objectTs.ts
@@ -42,3 +42,67 @@ type brew = {
 
 let coffee = { brewTime: 6, beans: "american" };
 let chaiBrew: brew = coffee;
+
+//PARTIAL
+
+type chai = {
+  name: string;
+  price: number;
+  isHot?: boolean;
+};
+
+//partial makes everything optional (all the properties)
+const updateChai = (updates: Partial<chai>) => {
+  console.log("msg", updates);
+};
+
+updateChai({ name: "masala" });
+updateChai({ isHot: false });
+updateChai({}); // this wont give error
+
+//REQUIRED
+
+type chaiOrder = {
+  name?: string;
+  price?: number;
+  isHot?: boolean;
+};
+
+//even though everything is optional in type if you use required then you have to give every value
+const placeOrder = (order: Required<chaiOrder>) => {
+  console.log("msg", order);
+};
+
+placeOrder({
+  name: "ginger",
+  price: 200,
+  isHot: true,
+});
+
+type newChai = {
+  name: string;
+  price: number;
+  isHot?: boolean;
+  ingredients: string[];
+};
+
+// type basicChoice = Pick< newChai, 'name' | 'price'>
+type basicChoice = Omit<newChai, "ingredients">; // ingredients will be hidden now
+
+const orderChai: basicChoice = {
+  name: "ginger",
+  price: 30,
+};
+
+//splitting
+type Item = {
+  name: string;
+  price: number;
+};
+
+type Address = { streat: string; pin: number };
+type orders = {
+  id: string;
+  items: Item[];
+  address: Address;
+};
